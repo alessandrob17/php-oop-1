e7fa8e4b3a15e36210c0f1724156b8c1e9e066ee
alessandrob17 <a.baldipardi@gmail.com>
2023-03-30 16:51:28 +0200

    FEAT construct

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
     public $titolo;
     public $genere;
     public $anno;
+
+    function __construct($_titolo,$_genere,$_anno) {
+    
+        $this->titolo = $_titolo;
+        $this->genere = $genere;
+        $this->anno = $anno;
+
+
+    }
  }
 
 
