a80ae0ca3114625e6ffe1e76b3c782068085b06e
alessandrob17 <a.baldipardi@gmail.com>
2023-03-31 16:21:27 +0200

    FEAT method

--- index.php
+++ index.php
@@ -10,10 +10,25 @@
         $this->titolo = $_titolo;
         $this->genere = $genere;
         $this->anno = $anno;
+        
 
 
     }
+
+    public function getMovie () {
+        return 'Il titolo del film è:' .$this->titolo . 'Genere:' .$this->genere . 'Anno pubblicazione:' .$this->anno
+    }
  }
+
+ $movie1 = new Movie ('The whale', 'Drammatico', '2023' );
+
+ $movie2 = new Movie ('Joohn Wick', 'Azione', '2014' );
+
+
+ var_dump($movie1);
+
+ var_dump($movie2)
+
 
 
 ?>
